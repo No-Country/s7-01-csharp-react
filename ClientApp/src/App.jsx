4a74ca203dce347022df455531e46ee2d917cd14
luisvelark <velark07@gmail.com>
2023-03-30 15:58:46 -0300

    build(front): instalación de tailwind css

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -1,42 +1,24 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <button onClick={() => setCount(count => count + 1)}>
-            count is: {count} cero
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> 
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div class="w-full">
+      <nav class="mx-auto flex justify-between">
+        <div class="text-2xl">
+          <img src="" alt="Logo" />
+        </div>
+        <div class="text-[20px] bg-luis">Hamburguesa</div>
+      </nav>
+      <div class="mt-6 p-2">
+        <h3 class="text-[16px] font-semibold">Mis Mascotas(5)</h3>
+      </div>
+      <div class="mt-6 flex w-full justify-around md:bg-black">
+        <div></div>
+        <div class="h-20 w-20 rounded-full border-4 bg-green-500 hover:border-purple-700 hover:scale-125"></div>
+        <div class="h-20 w-20 rounded-full border bg-green-500 hover:border-purple-700"></div>
+        <div class="h-20 w-20 rounded-full border bg-green-500 hover:border-purple-700"></div>
+        <div class="h-20 w-20 rounded-full border bg-green-500 hover:border-purple-700"></div>
+      </div>
     </div>
   );
 };
