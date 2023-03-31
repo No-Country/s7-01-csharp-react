c2ee4662c3ff6e9fcdf8dd97f82641823c27be79
DnTo <danto@hotmail.com>
2023-03-31 01:10:54 -0500

    add-react-dom-ruter

--- ClientApp/src/index.jsx
+++ ClientApp/src/index.jsx
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter } from 'react-router-dom';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+//root.render(
+//  <React.StrictMode>
+//    <App />
+//  </React.StrictMode>
+//);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <BrowserRouter basename={baseUrl}>
+        <App />
+    </BrowserRouter>);
+
+
+serviceWorkerRegistration.unregister();
+reportWebVitals();
