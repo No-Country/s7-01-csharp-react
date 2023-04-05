ce86c222d1c3d65ad196a2b95e7be034f244d36a
DnTo <danto@hotmail.com>
2023-04-04 22:41:52 -0500

    Add-Recordatorio Entity

---
+++ Api/DataAccess/Models/Recordatorio.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace s7_01.Api.DataAccess.Models
+{
+    public class Recordatorio
+    {
+        public int Id{ get; set; }
+
+        public int CreadorId { get; set; }
+
+        public int DestinatarioId { get; set; }
+
+        public DateTime  FechaCreacion { get; set; } = DateTime.Now;
+
+        public DateTime  FechaNotificacion { get; set; }
+
+        public int DiasPreviosNotificacion { get; set; }
+
+        public bool EsEmailEnviado { get; set; }
+
+        public bool EsSMSEnviado { get; set; }
+
+        public bool EsPushNotificationEnviada { get; set; }
+
+        public string Mensaje { get; set; }
+
+        public bool EsLeido { get; set; }
+    }
+}
