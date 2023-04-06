473caee708bdd10d7ab3776bfed946522a85a5cc
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2023-04-06 08:52:52 -0300

    agregando imagenes al proyecto y vista mobile home

--- ClientApp/src/components/Layouts/Navbar.jsx
+++ ClientApp/src/components/Layouts/Navbar.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import {Dog, UserCircle} from "@phosphor-icons/react";
+import Logo from "../../assets/images/Logo.svg"
+import profilePic from "../../assets/images/profilePic.png"
 
 const Navbar = () => {
-    return (
-        
-            <nav className="mx-auto flex justify-between w-full bg-slate-200">
+    return (        
+            <nav className="mx-auto flex justify-between w-full ">
                 <div className="text-2xl cursor-pointer">
-                   <Dog size={40} />
+                  <img src={Logo} alt="Logo" className='w-[130px] h-[32] mt-2 ml-2 '/>
                 </div>
-                <div className=" cursor-pointer bg-gree">
-                    <UserCircle size={40}/>
+                <div className="cursor-pointer">
+                    <img src={profilePic} alt="profilePic" className='w-8 h-8 rounded-full mt-2 mr-2'/>
                 </div>
-            </nav>
-
-        
+            </nav>        
     )
 }
 
