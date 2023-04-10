7fe5c74294497edbc37322ad2f05135ad362e06e
Exequiel65 <marcos97britos@gmail.com>
2023-04-09 21:20:53 -0300

    add methods get mascota and get Mascotas for propietarie

--- Api/Controllers/MascotaController.cs
+++ Api/Controllers/MascotaController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using s7_01.Api.Contracts.Repositories;
 using s7_01.Api.DataAccess.Models;
+using s7_01.Api.Dtos;
 
 namespace s7_01.Api.Controllers
 {
@@ -9,17 +10,37 @@ namespace s7_01.Api.Controllers
     public class MascotaController : Controller
     {
         private readonly IMascotaRepository _mascotaRepository;
+        private readonly IMascotaPropietarioRepository _mascotaPropietarioRepository;
 
-        public MascotaController(IMascotaRepository mascotaRepository)
+        public MascotaController(IMascotaRepository mascotaRepository, IMascotaPropietarioRepository mascotaPropietarioRepository)
         {
             _mascotaRepository = mascotaRepository;
+            _mascotaPropietarioRepository = mascotaPropietarioRepository;
         }
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Mascota>>> Index()
+        public async Task<ActionResult<IEnumerable<MascotaPropietario>>> Index()
         {
-            var mascota = _mascotaRepository.GetAll();
+            var mascotaPropietarios = _mascotaPropietarioRepository.GetAll();
 
-            return Ok(mascota);
+            return Ok(mascotaPropietarios);
+        }
+
+        [HttpGet("getForPropietarie/{id}")]
+        public async Task<ActionResult<MascotaXPropietarioDto>> getForPropietarie(int id)
+        {
+
+            var mascotaPropietarios = _mascotaPropietarioRepository.Find(m => m.PropietarioId == id);
+            List<Mascota> mascotas = new List<Mascota>();
+            foreach (var mascotaXPropietario in mascotaPropietarios)
+            {
+                mascotas.Add(mascotaXPropietario.Mascota);
+            }
+            MascotaXPropietarioDto mascotaXPropietarioDto = new MascotaXPropietarioDto() { Mascotas = mascotas };
+
+
+
+            return Ok(mascotaXPropietarioDto);
         }
     }
 }
+
--- Api/Repositories/MascotaPropietarioRepository.cs
+++ Api/Repositories/MascotaPropietarioRepository.cs
@@ -2,6 +2,7 @@
 using s7_01.Api.Contracts.Repositories;
 using s7_01.Api.DataAccess;
 using s7_01.Api.DataAccess.Models;
+using System.Linq.Expressions;
 
 namespace s7_01.Api.Repositories
 {
@@ -9,6 +10,18 @@ namespace s7_01.Api.Repositories
     {
         public MascotaPropietarioRepository(VeterinariaContext context) : base(context)
         {
+            
+        }
+        public IEnumerable<MascotaPropietario> GetAll()
+        {
+            return _context.Set<MascotaPropietario>()
+                .Include(m => m.Propietario)
+                .Include(m => m.Mascota)
+                .ToList();
+        }
+        public IEnumerable<MascotaPropietario> Find(Expression<Func<MascotaPropietario, bool>> expression)
+        {
+            return _context.Set<MascotaPropietario>().Where(expression).Include(m => m.Mascota).ToList();
         }
     }
 }
