f075bc5c18953421e9660f51b65265a4dd119c59
luisvelark <velark07@gmail.com>
2023-04-13 12:21:20 -0300

    style(front):agregar borde a la información

--- ClientApp/src/components/common/Information/index.jsx
+++ ClientApp/src/components/common/Information/index.jsx
@@ -1,8 +1,6 @@
 export function Information({ className, children }) {
   return (
-    <div
-      className={`border-0 p-0 border-solid border-[#999CA0] rounded-lg flex flex-col gap-2`}
-    >
+    <div className={`border-solid rounded-lg flex flex-col gap-2 ${className}`}>
       {children}
     </div>
   );
