14ced14cf8814a3f6ddd0581045ba85d2e5b2a99
luisvelark <velark07@gmail.com>
2023-04-13 12:35:59 -0300

    feat(front):agregar imagen en miniatura en previsualizacion de imagen

--- ClientApp/src/components/ContentImage/index.jsx
+++ ClientApp/src/components/ContentImage/index.jsx
@@ -4,7 +4,13 @@ export function ContentImage({ content }) {
   return (
     <div className="bg-white mt-2 px-4 py-3 border border-dashed border-gray-50 rounded-lg flex flex-col gap-2 content-center items-center">
       <div className="flex flex-row content-center items-center gap-2">
-        <div className="bg-gray-50 w-8 h-8"></div>
+        <div className="w-8 h-8">
+          <img
+            className="w-full h-full object-cover object-center"
+            src="https://picsum.photos/200/300?random=1"
+            alt=""
+          />
+        </div>
         <p className="text-[#888888] not-italic font-normal text-sm">
           Agrega una imagen de tu mascota
         </p>
