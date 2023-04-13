88781d89ae71077cb200c0ac3e9b2232c570686e
luisvelark <velark07@gmail.com>
2023-04-13 15:50:42 -0300

    style(front): realizar responsive de vista agregar mascotas

--- ClientApp/src/pages/AddPet/index.jsx
+++ ClientApp/src/pages/AddPet/index.jsx
@@ -13,8 +13,8 @@ export function AddPet() {
       <header>
         <Navbar />
       </header>
-      <main>
-        <section className="m-4">
+      <main className="grid place-items-center h-screen">
+        <section className="m-4 md:w-2/4">
           <Information className="border-0 p-0">
             <Title className="text-lg" content="Agregar mascota" />
             <Description
@@ -23,39 +23,39 @@ export function AddPet() {
             />
           </Information>
         </section>
-        <section className="m-4">
+        <section className="m-4 md:w-2/4">
           <Subtitle content="Imagen (opcional)" />
           <ContentImage />
         </section>
-        <section className="m-4">
+        <section className="m-4 md:w-2/4">
           <Subtitle content="Nombre" />
           <Input type="text" placeholder="Nombre de tu mascota" />
         </section>
-        <section className="m-4">
+        <section className="m-4 md:w-2/4">
           <Subtitle content="Especie" />
           <Select />
         </section>
-        <section className="m-4">
+        <section className="m-4 md:w-2/4">
           <Subtitle content="Raza" />
           <Input type="text" placeholder="Raza de tu mascota" />
         </section>
-        <section className="m-4">
+        <section className="m-4 md:w-2/4">
           <Subtitle content="Peso" />
           <Input type="text" placeholder="Peso de tu mascota" />
         </section>
-        <section className="m-4">
+        <section className="m-4 md:w-2/4">
           <Subtitle content="Fecha de nacimiento" />
           <Input type="date" />
         </section>
-        <section className="m-4">
+        <section className="m-4 md:w-2/4">
           <Subtitle content="Sexo" />
           <Radio label1="Hembra" label2="Macho" label3="Otro" name="sexo" />
         </section>
-        <section className="m-4">
+        <section className="m-4 md:w-2/4">
           <Subtitle content="Castración" />
           <Radio label1="Si" label2="No" label3="No lo sé" name="castracion" />
         </section>
-        <section className="m-4 mb-7">
+        <section className="m-4 md:w-2/4 mb-7">
           <Button
             className="w-full text-base text-white bg-primary-100"
             text="Agregar mascota"
