84bb0ad6012714906873fa13c8ff52f14b8972b6
luisvelark <velark07@gmail.com>
2023-04-14 14:24:17 -0300

    style(front): realizar responsive de componente image

--- ClientApp/src/components/common/Button/index.jsx
+++ ClientApp/src/components/common/Button/index.jsx
@@ -7,5 +7,3 @@ export function Button({ className, text }) {
     </button>
   );
 }
-
-// className={`border-[1px] border-solid border-gray-100 rounded-lg flex justify-center items-center p-2 focus:ring active:transform active:translate-y-1 text-black not-italic font-normal text-sm ${className}`
--- ClientApp/src/pages/AddPet/index.jsx
+++ ClientApp/src/pages/AddPet/index.jsx
@@ -13,8 +13,8 @@ export function AddPet() {
       <header>
         <Navbar />
       </header>
-      <main className="grid place-items-center h-screen">
-        <section className="m-4 md:w-2/4">
+      <main className="flex flex-col gap-0 items-center justify-center lg:ml-28 lg:items-start">
+        <section className="m-4 max-w-3xl min-w-[20em] w-11/12">
           <Information className="border-0 p-0">
             <Title className="text-lg" content="Agregar mascota" />
             <Description
@@ -23,39 +23,39 @@ export function AddPet() {
             />
           </Information>
         </section>
-        <section className="m-4 md:w-2/4">
+        <section className="m-4 max-w-3xl min-w-[20em] w-11/12 flex flex-col">
           <Subtitle content="Imagen (opcional)" />
           <ContentImage />
         </section>
-        <section className="m-4 md:w-2/4">
+        <section className="m-4 max-w-3xl min-w-[20em] w-11/12">
           <Subtitle content="Nombre" />
           <Input type="text" placeholder="Nombre de tu mascota" />
         </section>
-        <section className="m-4 md:w-2/4">
+        <section className="m-4 max-w-3xl min-w-[20em] w-11/12">
           <Subtitle content="Especie" />
           <Select />
         </section>
-        <section className="m-4 md:w-2/4">
+        <section className="m-4 max-w-3xl min-w-[20em] w-11/12">
           <Subtitle content="Raza" />
           <Input type="text" placeholder="Raza de tu mascota" />
         </section>
-        <section className="m-4 md:w-2/4">
+        <section className="m-4  max-w-3xl min-w-[20em] w-11/12">
           <Subtitle content="Peso" />
           <Input type="text" placeholder="Peso de tu mascota" />
         </section>
-        <section className="m-4 md:w-2/4">
+        <section className="m-4  max-w-3xl min-w-[20em] w-11/12">
           <Subtitle content="Fecha de nacimiento" />
           <Input type="date" />
         </section>
-        <section className="m-4 md:w-2/4">
+        <section className="m-4  max-w-3xl min-w-[20em] w-11/12">
           <Subtitle content="Sexo" />
           <Radio label1="Hembra" label2="Macho" label3="Otro" name="sexo" />
         </section>
-        <section className="m-4 md:w-2/4">
+        <section className="m-4  max-w-3xl min-w-[20em] w-11/12">
           <Subtitle content="Castración" />
           <Radio label1="Si" label2="No" label3="No lo sé" name="castracion" />
         </section>
-        <section className="m-4 md:w-2/4 mb-7">
+        <section className="m-4  max-w-3xl min-w-[20em] w-11/12">
           <Button
             className="w-full text-base text-white bg-primary-100"
             text="Agregar mascota"
