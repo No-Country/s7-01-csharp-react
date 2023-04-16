088789131e46df57276241b8edcfaebf70ee61c0
DnTo <danto@hotmail.com>
2023-04-16 00:15:26 -0500

    Fix HistoriaClinica dependency

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ using s7_01.Api.Extensions;
 using s7_01.Api.Services.Email;
 using System.ComponentModel;
 using System.Reflection;
-
+using s7_01.Api.DataAccess.Seeds;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -70,25 +70,31 @@ builder.Services.AddScoped<IGenericRepository<Servicio>, ServicioRepository>();
 builder.Services.AddScoped<IServicioRepository, ServicioRepository>();
 builder.Services.AddScoped<IServicioService, ServicioService>();
 
+builder.Services.AddScoped<IHistoriaClinicaService, HistoriaClinicaService>();
+builder.Services.AddScoped<IHistoriaClinicaRepository, HistoriaClinicaRepository>();
+builder.Services.AddScoped<IGenericRepository<HistoriaClinica>, HistoriaClinicaRepository>();
 
 
 builder.Services.AddScoped<IGenericRepository<Vacuna>, VacunaRepository>();
 builder.Services.AddScoped<IVacunaService, VacunaService>();
-
-var app = builder.Build();
-
-//using (var serviceScope = app.Services.CreateScope())
-//{
-//    var services = serviceScope.ServiceProvider;
-//    var context = services.GetRequiredService<VeterinariaContext>();
-//    context.Database.EnsureCreated();
-//}
 
 builder.Services.AddCors(policyBuilder =>
     policyBuilder.AddDefaultPolicy(policy =>
         policy.WithOrigins("*").AllowAnyHeader().AllowAnyHeader())
 );
 var app = builder.Build();
+
+using (var serviceScope = app.Services.CreateScope())
+{
+    var services = serviceScope.ServiceProvider;
+    var context = services.GetRequiredService<VeterinariaContext>();
+   //TODO context.Database.EnsureDeleted();
+    var isCreated = context.Database.EnsureCreated();
+    if (isCreated)
+        SeedGraph.Seed(context);
+}
+
+
 app.UseCors();
 
 // Configure the HTTP request pipeline.
