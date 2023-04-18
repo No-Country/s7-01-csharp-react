3a60cdf0db1eb7eb0db8528e7b41bac02f7e8a66
luisvelark <velark07@gmail.com>
2023-04-17 21:18:24 -0300

    style(front):corregir background de secciones servicios,redes,otras clinicas

--- ClientApp/src/components/ServiceClinic/index.jsx
+++ ClientApp/src/components/ServiceClinic/index.jsx
@@ -4,7 +4,7 @@ services;
 
 export function ServiceClinic() {
   return (
-    <div className="p-4 border-[1px] border-solid border-[#999CA0] rounded-lg flex flex-col gap-2">
+    <div className="p-4 bg-white rounded-lg flex flex-col gap-2">
       <h2 className="text-[#010B1D] not-italic font-medium text-xl">
         Servicios ofrecidos
       </h2>
