aca0c765fa2d510625ef751a8ad51e40cb2bbc00
Exequiel65 <marcos97britos@gmail.com>
2023-04-20 14:14:04 -0300

    add changes veterinary

--- ClientApp/src/pages/PetVet/ThirdBlock.jsx
+++ ClientApp/src/pages/PetVet/ThirdBlock.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { addMascotaVeterinary } from "../../services/addMascotaVeterinary";
 
 const ThirdBlock = ({ mascotas }) => {
+  let navigate = useNavigate()
   if (!mascotas) {
     return <div></div>;
+  }
+  const handleClick = async (m)=>{
+    let response = await addMascotaVeterinary(1, m.id)
+    navigate('/app/veterinary')
   }
   return (
     <div className="mt-4 block md:grid md:grid-cols-4 md:justify-between md:gap-x-16 md:gap-y-2">
@@ -18,7 +25,7 @@ const ThirdBlock = ({ mascotas }) => {
           />
           <p>{m.nombre}</p>
           <p>{m.especie}</p>
-          <button className="w-[191px] h-[48px] mt-2 mx-auto px-4 py-3 rounded-md border-primary-100 border bg-gray-50 flex justify-center gap-2">
+          <button type="submit" className="w-[191px] h-[48px] mt-2 mx-auto px-4 py-3 rounded-md border-primary-100 border bg-gray-50 flex justify-center gap-2"  onClick={()=> handleClick(m)} >
             <p className="text-primary-100">Añadir Paciente</p>
           </button>
         </div>
